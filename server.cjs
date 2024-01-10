57863332ed6a352c0149efe8ae9621a1c0872c87
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-10 15:22:17 -0800

    Added many files and updated server file

---
+++ server.cjs
@@ -0,0 +1,40 @@
+// defining the server port
+const port = 5000
+
+// initializing installed dependencies
+const express = require('express')
+require('dotenv').config()
+const axios = require('axios')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+// listening for port 5000
+app.listen(5000, () => console.log(`Server is running on ${port}`))
+
+function getRequest(options, res, req) {
+
+    axios.request(options)
+        .then(function (response) {
+            res.json(response.data)
+        })
+        .then(data => console.log(data))
+        .catch(function (error) {
+            console.error(error)
+        })
+
+}
+
+// API request
+app.get('/', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.thedogapi.com/v1/images/search',
+        headers: {
+            'x-api-key': process.env.VITE_SOME_KEY,
+            'X-api-host': 'api.thedogapi.com'
+        }
+    }
+
+    getRequest(options, res, req)
+})
