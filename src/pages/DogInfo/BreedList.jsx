c77a247daaaa9e8635c34f48996d3925ce7e4317
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-10 22:52:05 -0800

    Made many changes
    Added scripts to package .json to run frontend and backend together.
    
    Added newer components and routes.

---
+++ src/pages/DogInfo/BreedList.jsx
@@ -0,0 +1,31 @@
+import axios from "axios"
+import React from "react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+export default function BreedList() {
+
+    const [loading, setLoading] = useState(false)
+    const [dogData, setDogData] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+
+        const options = {
+            method: "GET",
+            url: "http://localhost:5000"
+        }
+
+        axios.request(options)
+            .then(response => {
+                console.log(response.data)
+                setDogData(response.data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+    }, [])
+
+    return <h1>Breeds here</h1>
+}
