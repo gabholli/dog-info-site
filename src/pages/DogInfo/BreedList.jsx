7709e5f92e6bb12759af725f6ad9adfaa9346645
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-12 15:55:28 -0800

    Removed flex as display type for breeds list

--- src/pages/DogInfo/BreedList.jsx
+++ src/pages/DogInfo/BreedList.jsx
@@ -8,6 +8,10 @@ export default function BreedList() {
     const [loading, setLoading] = useState(false)
     const [dogData, setDogData] = useState([])
 
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage, setItemsPerPage] = useState(10)
+    const [activePage, setActivePage] = useState(1)
+
     useEffect(() => {
         setLoading(true)
 
@@ -30,20 +34,55 @@ export default function BreedList() {
     const dogBreedList = dogData?.map(dog => {
         return (
             <div
-                className="text-center flex items-center justify-center"
+                className="text-center"
                 key={dog.id}>
                 <Link
-                    className="hover:underline"
+                    className="hover:underline text-2xl"
                 >
                     <img
-                        className="size-80 object-cover"
+                        className="size-80 object-cover mb-7"
                         src={dog.image.url}>
 
                     </img>
-
                     {dog.name}
                 </Link>
             </div>
+        )
+    })
+
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = dogBreedList.slice(indexOfFirstItem, indexOfLastItem)
+    const pageNumbers = []
+
+    function paginate(pageNumber) {
+        return setCurrentPage(pageNumber)
+    }
+
+    for (let i = 1; i <= Math.ceil(dogBreedList.length / itemsPerPage); i++) {
+        pageNumbers.push(i)
+    }
+
+    function handlePageClick(number) {
+        setActivePage(number)
+        paginate(number)
+    }
+
+    const tenItemList = pageNumbers.map(number => {
+        return (
+            <li
+                className="hover:underline"
+                key={number}
+            >
+                <a
+                    onClick={() => {
+                        handlePageClick(number)
+                        paginate(number)
+                    }}
+                >
+                    {number}
+                </a>
+            </li>
         )
     })
 
@@ -51,8 +90,19 @@ export default function BreedList() {
         <div className="flex flex-col justify-center items-center p-8 gap-y-8">
             <h1 className="font-bold text-2xl">Select a breed:</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4
-                gap-4">
-                {dogBreedList}
+                gap-8">
+                {currentItems}
+            </div>
+            <div>
+                <nav className="flex flex-col gap-y-6">
+                    <h1 className="font-bold text-xl text-center">Pages:</h1>
+                    <ul className="list-none flex flex-wrap justify-center gap-x-8 md:gap-x-6 
+                                    gap-y-4 cursor-pointer">
+                        {
+                            tenItemList
+                        }
+                    </ul>
+                </nav>
             </div>
         </div>
     )
