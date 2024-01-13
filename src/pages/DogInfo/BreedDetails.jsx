22159a572a6fd430714a1a43fe4808dbc37d05c1
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-13 02:28:37 -0800

    Added loading and error handling for details page

--- src/pages/DogInfo/BreedDetails.jsx
+++ src/pages/DogInfo/BreedDetails.jsx
@@ -32,6 +32,28 @@ export default function BreedDetails() {
 
     }, [id])
 
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path
+                    fill="currentColor" d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
+                    <animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite"
+                        type="rotate" values="0 12 12;360 12 12" /></path></svg>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className="flex flex-col justify-center items-center">
+                <h1 className=" text-3xl mb-8 text-center mt-8">There was an error loading this page...</h1>
+                <Link to="/" className="bg-sky-300 px-4 py-2 rounded text-xl hover:underline">
+                    Return to home
+                </Link>
+            </div>
+        )
+    }
+
     return (
         <div className="flex flex-col gap-y-12 p-8 justify-center items-center">
             <Link
@@ -40,7 +62,7 @@ export default function BreedDetails() {
                 relative="path">
                 &larr; <span className="text-3xl">Back to breeds list</span>
             </Link>
-            {dogData.name && <h1 className="text-4xl underline text-center">Facts about {dogData.name}s:</h1>}
+            <h1 className="text-4xl underline text-center">Facts about {dogData.name}s:</h1>
             <div className="md:hidden flex flex-col justify-center items-center text-center px-4 
             py-8 gap-y-10">
                 {dogData.bred_for && <h2 className="text-3xl underline">Reasons for breeding:</h2>}
