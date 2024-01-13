19eb00642e744e2de78b8d1681d422bcbb0528f8
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-13 00:57:12 -0800

    Added dog details to component and created link to breeds list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,12 @@ export default function Header() {
     return (
         <header className="bg-sky-200 h-20 flex justify-center items-center
             text-xl">
-            <div className="flex gap-10 hover:underline">
-                <Link to="/">Home</Link>
+            <div className="flex gap-10">
+                <Link
+                    className="hover:underline"
+                    to="/">
+                    Home
+                </Link>
             </div>
         </header>
     )
--- src/pages/DogInfo/BreedDetails.jsx
+++ src/pages/DogInfo/BreedDetails.jsx
@@ -9,8 +9,6 @@ export default function BreedDetails() {
     const [error, setError] = useState(null)
     const [dogData, setDogData] = useState([])
 
-    console.log(id)
-
     useEffect(() => {
         setLoading(true)
 
@@ -35,6 +33,29 @@ export default function BreedDetails() {
     }, [id])
 
     return (
-        <h1>Facts about </h1>
+        <div className="flex flex-col justify-center items-center text-center px-4 
+            py-8 gap-y-10">
+            <Link
+                className="hover:underline"
+                to=".."
+                relative="path">
+                &larr; <span className="text-3xl">Back to breeds list</span>
+            </Link>
+            <h1 className="text-4xl underline">Facts about {dogData.name}s:</h1>
+            {dogData.bred_for && <h2 className="text-3xl underline">Reasons for breeding:</h2>}
+            {dogData.bred_for && <p className="text-2xl">{dogData.bred_for}</p>}
+            {dogData.breed_group && <h2 className="text-3xl underline">Breed group:</h2>}
+            {dogData.breed_group && <p className="text-3xl">{dogData.breed_group}</p>}
+            {dogData.height?.imperial && <h2 className="text-3xl underline">Height in inches:</h2>}
+            {dogData.height?.imperial && <p className="text-2xl">{dogData.height?.imperial}</p>}
+            {dogData.life_span && <h2 className="text-3xl underline">Average life span:</h2>}
+            {dogData.life_span && <p className="text-2xl">{dogData.life_span}</p>}
+            {dogData.origin && <h2 className="text-3xl underline">Place(s) of origin:</h2>}
+            {dogData.origin && <p className="text-2xl">{dogData.origin}</p>}
+            {dogData.temperament && <h2 className="text-3xl underline">Temperament:</h2>}
+            {dogData.temperament && <p className="text-2xl">{dogData.temperament}</p>}
+            {dogData.weight?.imperial && <h2 className="text-3xl underline">Weight in pounds:</h2>}
+            {dogData.weight?.imperial && <p className="text-2xl">{dogData.weight?.imperial}</p>}
+        </div>
     )
 }
