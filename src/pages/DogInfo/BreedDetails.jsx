1597ce765f04163c666a8146fd72d1b080202131
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-13 01:24:48 -0800

    Inserted dog breed data into a table for tablets and above

--- src/pages/DogInfo/BreedDetails.jsx
+++ src/pages/DogInfo/BreedDetails.jsx
@@ -33,29 +33,61 @@ export default function BreedDetails() {
     }, [id])
 
     return (
-        <div className="flex flex-col justify-center items-center text-center px-4 
-            py-8 gap-y-10">
+        <div className="flex flex-col gap-y-12 p-8 justify-center items-center">
             <Link
-                className="hover:underline"
+                className="hover:underline text-center"
                 to=".."
                 relative="path">
                 &larr; <span className="text-3xl">Back to breeds list</span>
             </Link>
-            <h1 className="text-4xl underline">Facts about {dogData.name}s:</h1>
-            {dogData.bred_for && <h2 className="text-3xl underline">Reasons for breeding:</h2>}
-            {dogData.bred_for && <p className="text-2xl">{dogData.bred_for}</p>}
-            {dogData.breed_group && <h2 className="text-3xl underline">Breed group:</h2>}
-            {dogData.breed_group && <p className="text-3xl">{dogData.breed_group}</p>}
-            {dogData.height?.imperial && <h2 className="text-3xl underline">Height in inches:</h2>}
-            {dogData.height?.imperial && <p className="text-2xl">{dogData.height?.imperial}</p>}
-            {dogData.life_span && <h2 className="text-3xl underline">Average life span:</h2>}
-            {dogData.life_span && <p className="text-2xl">{dogData.life_span}</p>}
-            {dogData.origin && <h2 className="text-3xl underline">Place(s) of origin:</h2>}
-            {dogData.origin && <p className="text-2xl">{dogData.origin}</p>}
-            {dogData.temperament && <h2 className="text-3xl underline">Temperament:</h2>}
-            {dogData.temperament && <p className="text-2xl">{dogData.temperament}</p>}
-            {dogData.weight?.imperial && <h2 className="text-3xl underline">Weight in pounds:</h2>}
-            {dogData.weight?.imperial && <p className="text-2xl">{dogData.weight?.imperial}</p>}
+            <h1 className="text-4xl underline text-center">Facts about {dogData.name}s:</h1>
+            <div className="md:hidden flex flex-col justify-center items-center text-center px-4 
+            py-8 gap-y-10">
+                {dogData.bred_for && <h2 className="text-3xl underline">Reasons for breeding:</h2>}
+                {dogData.bred_for && <p className="text-2xl">{dogData.bred_for}</p>}
+                {dogData.breed_group && <h2 className="text-3xl underline">Breed group:</h2>}
+                {dogData.breed_group && <p className="text-3xl">{dogData.breed_group}</p>}
+                {dogData.height?.imperial && <h2 className="text-3xl underline">Height in inches:</h2>}
+                {dogData.height?.imperial && <p className="text-2xl">{dogData.height?.imperial}</p>}
+                {dogData.life_span && <h2 className="text-3xl underline">Average life span:</h2>}
+                {dogData.life_span && <p className="text-2xl">{dogData.life_span}</p>}
+                {dogData.origin && <h2 className="text-3xl underline">Place(s) of origin:</h2>}
+                {dogData.origin && <p className="text-2xl">{dogData.origin}</p>}
+                {dogData.temperament && <h2 className="text-3xl underline">Temperament:</h2>}
+                {dogData.temperament && <p className="text-2xl">{dogData.temperament}</p>}
+                {dogData.weight?.imperial && <h2 className="text-3xl underline">Weight in pounds:</h2>}
+                {dogData.weight?.imperial && <p className="text-2xl">{dogData.weight?.imperial}</p>}
+            </div>
+            <table className="max-[767px]:hidden table-fixed border-collapse border border-black">
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Reasons for breeding:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.bred_for}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Breed group:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.breed_group}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Height in inches:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.height?.imperial}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Average life span:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.life_span}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Place(s) of origin:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.origin}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Temperament:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.temperament}</td>
+                </tr>
+                <tr>
+                    <td className="border border-black px-8 py-4 text-2xl">Weight in pounds:</td>
+                    <td className="border border-black px-8 py-4 text-2xl">{dogData.weight?.imperial}</td>
+                </tr>
+            </table>
         </div>
     )
 }
