e4998154f3555d03fcd8ae1725ad462b6017bd16
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-13 17:24:21 -0800

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -64,3 +64,16 @@ app.get('/breeds/:id', (req, res) => {
 
     getRequest(options, res, req)
 })
+
+app.get('/images/search/', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: `https://api.thedogapi.com/v1/images/search?breed_id=${req.params.breedId}`,
+        headers: {
+            'x-api-key': process.env.VITE_SOME_KEY,
+            'X-api-host': 'api.thedogapi.com'
+        }
+    }
+
+    getRequest(options, res, req)
+})
